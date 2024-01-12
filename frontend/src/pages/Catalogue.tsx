b4306b0a27d0b299e9ba80256d6888647cb5a224
omar <o.wakeel@intouch.com>
2024-01-13 00:27:50 +0200

    feat: add react query

--- frontend/src/pages/Catalogue.tsx
+++ frontend/src/pages/Catalogue.tsx
@@ -1,36 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { ICatalogue } from '../utilities/interfaces/catalogue.interface'
 import { formatRequestDate, hashRequestHeader } from '../utilities/helpers'
+import { useQuery } from '@tanstack/react-query'
 
 const Catalogue = () => {
-    const [data, setData] = useState<Array<ICatalogue>>([])
 
-    useEffect(() => {
-        const path = 'items'
-        const date = formatRequestDate()
-        const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNdW5lcm8iLCJleHAiOjE3MDUxODI5NDMsInR5cGUiOiJBdXRob3JpemF0aW9uVG9rZW4iLCJjcmVhdGlvbkRhdGUiOjE3MDUwOTY1NDMsInVzZXJJZCI6MTEzLCJ2ZXJzaW9uIjoxfQ.0fsBbbFFlxeiqYVrEo2zrvOUuLZ7AR5fDu2RDfVzC6U'
+    const { data, isLoading, error } = useQuery<Array<ICatalogue>>({
+        queryFn: () => {
+            const path = 'items'
+            const date = formatRequestDate()
+            const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNdW5lcm8iLCJleHAiOjE3MDUxODI5NDMsInR5cGUiOiJBdXRob3JpemF0aW9uVG9rZW4iLCJjcmVhdGlvbkRhdGUiOjE3MDUwOTY1NDMsInVzZXJJZCI6MTEzLCJ2ZXJzaW9uIjoxfQ.0fsBbbFFlxeiqYVrEo2zrvOUuLZ7AR5fDu2RDfVzC6U'
 
-        const signature = path + 'GET' + date + token;
-        const secret = 'coding_challenge_1'
+            const signature = path + 'GET' + date + token;
+            const secret = 'coding_challenge_1'
 
-        const signatureHeader = hashRequestHeader(signature, secret)
+            const signatureHeader = hashRequestHeader(signature, secret)
 
-        fetch(`https://staging.giftlov.com/api/Base/${path}`, {
-            headers: {
-                'X-GIFTLOV-DATE': date,
-                'Accept': 'application/json',
-                'signature': signatureHeader,
-                'Authorization': token
-            }
-        })
-            .then((res) => res.json())
-            .then((data) => setData(data.items))
-    }, [])
+            return fetch(`https://staging.giftlov.com/api/Base/${path}`, {
+                headers: {
+                    'X-GIFTLOV-DATE': date,
+                    'Accept': 'application/json',
+                    'signature': signatureHeader,
+                    'Authorization': token
+                }
+            })
+                .then((res) => res.json())
+                .then((data) => data.items)
+        },
+        queryKey: ['catalogue'],
+    });
+
+    if (isLoading) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
+    if (error) {
+        return (
+            <p>Error</p>
+        )
+    }
 
     return (
         <div>
             <h1>Catalogue</h1>
-            {data.map((item) => (
+            {data?.map((item) => (
                 <div key={item.id}>
                     {item.brand}
                 </div>
