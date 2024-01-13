ae432c2729cd6a9936d160e16d22bc5b0743a90e
omar <o.wakeel@intouch.com>
2024-01-13 20:45:14 +0200

    feat: adjust header scroll animation

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -8,9 +8,11 @@ export default function Header() {
 
     useEffect(() => {
         const listener = () => {
-            if (window.scrollY > 140) {
+            if (window.scrollY > 40) {
                 setAnimateHeader(true);
-            } else setAnimateHeader(false);
+            } else {
+                setAnimateHeader(false);
+            }
         };
 
         window.addEventListener("scroll", listener);
@@ -22,7 +24,7 @@ export default function Header() {
 
     return (
         <header className={`w-full backdrop-filter backdrop-blur-lg bg-white/50 fixed z-20 transition-all ease-in-out duration-500 ${animateHeader && "shadow-xl"}`}>
-            <div className={`flex max-w-screen-xl py-4 ${animateHeader && "py-5"} mx-auto items-center justify-between px-8 transition-all ease-in-out duration-500`}>
+            <div className={`flex max-w-screen-xl ${animateHeader && "py-5"} mx-auto items-center justify-between px-8 transition-all ease-in-out duration-500`}>
                 <Link
                     to="/"
                     className="text-xl font-bold tracking-tighter text-indigo-400 pr-8"
